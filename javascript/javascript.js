be4843acd0fa3743bd5f937770d6b3116102a8aa
Bryn Miller <bryn-miller@users.noreply.github.com>
2023-12-08 11:28:15 -0700

    Website deploy final

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#hamburger-menu').click(function(){
+        $('#mobile-nav').fadeIn();
+    });
+    $('#close-menu').click(function(){
+        $('#mobile-nav').fadeOut();
+    });
+});
